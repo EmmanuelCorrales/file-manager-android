b7208bd7af4fc3f8f01de8149af630469f61a149
Emmanuel Corrales <emmanuelmcorrales@gmail.com>
2016-10-13 00:11:05 +0800

    Set an OnClickFileListener for FileBrowserFragment. Start an activity with intent to open a file.

--- app/src/main/java/com/emmanuelcorrales/filemanager/MainActivity.java
+++ app/src/main/java/com/emmanuelcorrales/filemanager/MainActivity.java
@@ -4,19 +4,47 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+
+import filebrowser.FileBrowserFragment;
+import filebrowser.Utils;
+
+public class MainActivity extends AppCompatActivity implements FileBrowserFragment.OnFileClickedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FileBrowserFragment fileBrowserFragment = getFileBrowserFragment();
+        if (fileBrowserFragment != null) {
+            fileBrowserFragment.setOnFileClickedListener(this);
+        }
     }
 
     @Override
     public void onBackPressed() {
+        FileBrowserFragment fileBrowserFragment = getFileBrowserFragment();
+        if (fileBrowserFragment != null) {
+            getFileBrowserFragment().navigateParentDirectory();
+        }
+    }
+
+    @Override
+    public void OnFileClicked(File file) {
+        try {
+            startActivity(Utils.createOpenFileIntent(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private FileBrowserFragment getFileBrowserFragment() {
         Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment);
         if (fragment != null) {
-            ((filebrowser.FileBrowserFragment) fragment).navigateParentDirectory();
+            return ((filebrowser.FileBrowserFragment) fragment);
         }
+        return null;
     }
 }
