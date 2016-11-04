6bd17aaaf8437ed16c5d396714395254067cb54c
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2016-11-04 20:44:42 +0800

    Call intent to install APK. Added icons for APK files.

--- app/src/main/java/com/emmanuelcorrales/filemanager/Utils.java
+++ app/src/main/java/com/emmanuelcorrales/filemanager/Utils.java
@@ -37,17 +37,15 @@ public class Utils {
         // Check what kind of file you are trying to open, by comparing the url with extensions.
         // When the if condition is matched, plugin sets the correct intent (mime) type,
         // so Android knew what application to use to open the file
-        if (file.toString().contains(".doc") || file.toString().contains(".docx")) {
-            // Word document
+        if (isDocument(file)) {
             intent.setDataAndType(uri, "application/msword");
         } else if (file.toString().contains(".pdf")) {
             // PDF file
             intent.setDataAndType(uri, "application/pdf");
-        } else if (file.toString().contains(".ppt") || file.toString().contains(".pptx")) {
+        } else if (isPowerPoint(file)) {
             // Powerpoint file
             intent.setDataAndType(uri, "application/vnd.ms-powerpoint");
-        } else if (file.toString().contains(".xls") || file.toString().contains(".xlsx")) {
-            // Excel file
+        } else if (isExcel(file)) {
             intent.setDataAndType(uri, "application/vnd.ms-excel");
         } else if (file.toString().contains(".zip") || file.toString().contains(".rar")) {
             // WAV audio file
@@ -55,25 +53,18 @@ public class Utils {
         } else if (file.toString().contains(".rtf")) {
             // RTF file
             intent.setDataAndType(uri, "application/rtf");
-        } else if (file.toString().contains(".wav") || file.toString().contains(".mp3")) {
-            // WAV audio file
+        } else if (isMusic(file)) {
             intent.setDataAndType(uri, "audio/x-wav");
         } else if (file.toString().contains(".gif")) {
-            // GIF file
             intent.setDataAndType(uri, "image/gif");
-        } else if (file.toString().contains(".jpg") || file.toString().contains(".jpeg")
-                || file.toString().contains(".png")) {
-            // JPG file
+        } else if (isImage(file)) {
             intent.setDataAndType(uri, "image/jpeg");
         } else if (file.toString().contains(".txt")) {
             // Text file
             intent.setDataAndType(uri, "text/plain");
-        } else if (file.toString().contains(".3gp") || file.toString().contains(".mpg")
-                || file.toString().contains(".mpeg") || file.toString().contains(".mpe")
-                || file.toString().contains(".mp4") || file.toString().contains(".avi")) {
-            // Video files
+        } else if (isVideo(file)) {
             intent.setDataAndType(uri, "video/*");
-        } else if (file.toString().contains(".apk")) {
+        } else if (isAPK(file)) {
             intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
         } else {
             //if you want you can also define the intent type for any other file
@@ -88,34 +79,62 @@ public class Utils {
     }
 
     public static int getFileDrawableResource(File file) {
-        if (file.toString().contains(".doc") || file.toString().contains(".docx")) {
+        if (isDocument(file)) {
             return R.drawable.ic_doc;
         } else if (file.toString().contains(".pdf")) {
             // PDF file
-        } else if (file.toString().contains(".ppt") || file.toString().contains(".pptx")) {
+        } else if (isPowerPoint(file)) {
             return R.drawable.ic_ppt;
-        } else if (file.toString().contains(".xls") || file.toString().contains(".xlsx")) {
+        } else if (isExcel(file)) {
             return R.drawable.ic_xls;
         } else if (file.toString().contains(".zip") || file.toString().contains(".rar")) {
             // WAV audio file
         } else if (file.toString().contains(".rtf")) {
             // RTF file
-        } else if (file.toString().contains(".wav") || file.toString().contains(".mp3")) {
+        } else if (isMusic(file)) {
             return R.drawable.ic_music;
         } else if (file.toString().contains(".gif")) {
             // GIF file
-        } else if (file.toString().contains(".jpg") || file.toString().contains(".jpeg")
-                || file.toString().contains(".png")) {
+        } else if (isImage(file)) {
             return R.drawable.ic_image;
         } else if (file.toString().contains(".txt")) {
             // Text file
-        } else if (file.toString().contains(".3gp") || file.toString().contains(".mpg")
-                || file.toString().contains(".mpeg") || file.toString().contains(".mpe")
-                || file.toString().contains(".mp4") || file.toString().contains(".avi")) {
+        } else if (isVideo(file)) {
             return R.drawable.ic_video;
-        } else if (file.toString().contains(".apk")) {
+        } else if (isAPK(file)) {
             return R.drawable.ic_apk;
         }
         return R.drawable.ic_file;
+    }
+
+    public static boolean isDocument(File file) {
+        return file.toString().contains(".doc") || file.toString().contains(".docx");
+    }
+
+    public static boolean isPowerPoint(File file) {
+        return file.toString().contains(".ppt") || file.toString().contains(".pptx");
+    }
+
+    public static boolean isExcel(File file) {
+        return file.toString().contains(".xls") || file.toString().contains(".xlsx");
+    }
+
+    public static boolean isMusic(File file) {
+        return file.toString().contains(".wav") || file.toString().contains(".mp3");
+    }
+
+    public static boolean isImage(File file) {
+        return file.toString().contains(".jpg") || file.toString().contains(".jpeg")
+                || file.toString().contains(".png");
+    }
+
+    public static boolean isVideo(File file) {
+        return file.toString().contains(".3gp") || file.toString().contains(".mpg")
+                || file.toString().contains(".mpeg") || file.toString().contains(".mpe")
+                || file.toString().contains(".mp4") || file.toString().contains(".avi");
+    }
+
+    public static boolean isAPK(File file) {
+        return file.toString().contains(".apk");
     }
 }
